cfcc5bcef0148f581567de93c47a4af3c9ff01a2
Wilmer Entena <wil.entena@gmail.com>
2018-08-06 09:44:44 +0200

    Revert "Welcome page edited"
    This reverts commit e90af008c9297e1fadabde6319e1e8cd08976bc8.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,85 +5,91 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Online DTR</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
     </head>
     <body>
-      <div class="container">
-        <br>
-        <div align="center"><img src="images/logo.png" width="200" height="auto" class="img-responsive"> </div>
-        <br>
-          <div class="row justify-content-center">
-              <div class="col-md-8">
-                  <div class="card">
-                      <div class="card-header">{{ __('Login') }}</div>
-                      <div class="card-body">
-                          <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
-                              @csrf
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
 
-                              <div class="form-group row">
-                                <div class="col-md-3 text-md-right"><i class="fas fa-user fa-2x"></i></div>
-                                  <div class="col-md-7">
-                                      <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-                                      @if ($errors->has('email'))
-                                          <span class="invalid-feedback" role="alert">
-                                              <strong>{{ $errors->first('email') }}</strong>
-                                          </span>
-                                      @endif
-                                  </div>
-                              </div>
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
 
-                              <div class="form-group row">
-                                  <div class="col-md-3 text-md-right"><i class="fas fa-lock fa-2x"></i></div>
-                                  <div class="col-md-7">
-                                      <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-                                      @if ($errors->has('password'))
-                                          <span class="invalid-feedback" role="alert">
-                                              <strong>{{ $errors->first('password') }}</strong>
-                                          </span>
-                                      @endif
-                                  </div>
-                              </div>
-
-                              <div class="form-group row">
-                                  <div class="col-md-6 offset-md-4">
-                                      <div class="form-check">
-                                          <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                          <label class="form-check-label" for="remember">
-                                              {{ __('Remember Me') }}
-                                          </label>
-                                      </div>
-                                  </div>
-                              </div>
-
-                              <div class="form-group row mb-0">
-                                  <div class="col-md-8 offset-md-4">
-                                      <button type="submit" class="btn btn-primary">
-                                          {{ __('Login') }}
-                                      </button>
-
-                                      <a class="btn btn-link" href="{{ route('password.request') }}">
-                                          {{ __('Forgot Your Password?') }}
-                                      </a>
-                                  </div>
-                              </div>
-                          </form>
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <br>
-          <div align="center"> Copyright &copy; <script type="text/javascript">document.write(new Date().getFullYear());</script><br/>Wilmer Entena <br>Business Academy Aarhus</div>
-      </div>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+                <div class="links">
+                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
+        </div>
     </body>
-
 </html>
