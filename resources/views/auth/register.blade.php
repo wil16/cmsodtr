dce92c3a9db9dd0ad7fee4d9eee103f28106f96d
Wilmer Entena <wil.entena@gmail.com>
2018-08-24 21:39:13 +0200

    Fixed comment in dropdown list

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -53,7 +53,7 @@
                             </div>
                         </div>
 
-                        //Dropdown list (to do - apply eloquent to make it dynamic)
+                        <!--Dropdown list (to do - apply eloquent to make it dynamic)-->
                         <div class="form-group row">
                             <label for="department_id" class="col-md-4 col-form-label text-md-right">{{ __('Department') }}</label>
                             <div class="col-md-6">
